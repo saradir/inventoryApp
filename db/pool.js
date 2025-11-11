8e723f7619f497ca34e9d24c51c37027efcb708f
saradir <120840462+saradir@users.noreply.github.com>
2025-11-12 00:19:19 +0200

    remove .env from repository

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,8 @@
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 const { Pool } = require('pg');
 
 module.exports = new Pool ({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_SSL === 'true' ? {rejectUnauthorized}: false,
+    ssl: process.env.DATABASE_SSL === 'true' ? {rejectUnauthorized:false}: false,
     });
